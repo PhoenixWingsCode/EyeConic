3a68ac193b1045d59fff8f544665ce43fe2082c9
PhoenixWingsCode <154915383+PhoenixWingsCode@users.noreply.github.com>
2024-03-16 00:22:49 +0530

    Add files via upload

---
+++ Password/PasswordUtility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+public class PasswordUtility
+{
+    public static string EncryptPassword(string password)
+    {
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return Convert.ToBase64String(hashedBytes);
+        }
+    }
+
+    public static bool VerifyPassword(string inputPassword, string hashedPassword)
+    {
+        string hashedInputPassword = EncryptPassword(inputPassword);
+        return hashedInputPassword == hashedPassword;
+    }
+}
